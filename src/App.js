fd43c2e33cf6432b0a42859ea51e0c1bab04d148
ttleonardo <ttleonardo@hotmail.com>
2020-01-09 10:39:53 -0600

    updating our app with shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <div > <HomePage/>  </div>
+    <div >
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
