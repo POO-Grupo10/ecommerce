68d8f6315c938ce80faedd60ed0ccc92b4c122e0
ttleonardo <ttleonardo@hotmail.com>
2020-01-28 08:56:56 -0600

    include the shop cart with redux

--- src/components/collection-items/collection-items.component.jsx
+++ src/components/collection-items/collection-items.component.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 import './collection-items.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { addItemsToCart } from '../../redux/cart/cart.actions';
 
-const CollectionItems = ({id, name, price, imageUrl}) => (
+const CollectionItems = ({item, addItem }) => {
+
+    const {name, price, imageUrl} = item;
+
+    return (
     <div className='collection-item'>
         <div 
         className='image'
@@ -13,7 +20,12 @@ const CollectionItems = ({id, name, price, imageUrl}) => (
                 <span className='name'>{ name }</span>
                 <span className='price'>{ price }</span>
             </div>
+             <CustomButton onClick={() => addItem(item)} inverted >Add to cart</CustomButton>
     </div>
-);
+)};
 
-export default CollectionItems;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItemsToCart(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItems);
